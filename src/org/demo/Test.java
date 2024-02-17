935b9f0e5b862f8d924a954fa03d696b23d98989
DhanushBhoobalan <dhanushbhoobalan@gmail.com>
2024-02-17 07:37:41 -0800

    new code

---
+++ src/org/demo/Test.java
@@ -0,0 +1,10 @@
+package org.demo;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("------------demo codes--------------");
+			System.out.println("--------------My Details--------------");
+			System.out.println("My Name : DhanushBhooalan");
+			System.out.println("My Course : Java-Selenium");
+	}
+}
